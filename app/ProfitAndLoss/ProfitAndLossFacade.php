add87fa63fee97c9d806ebd913103dcc027e11c0
mcgettrm <mcgettrick.michael@gmail.com>
2023-02-25 15:39:37 +0800

    Fleshing out facade prior to registering services with the container

--- app/ProfitAndLoss/ProfitAndLossFacade.php
+++ app/ProfitAndLoss/ProfitAndLossFacade.php
@@ -8,7 +8,22 @@ namespace App\ProfitAndLoss;
  */
 class ProfitAndLossFacade
 {
-    public function retrievePersistAndReturnProfitAndLoss(){
+    public function retrievePersistAndReturnProfitAndLoss():ProfitAndLossDTO {
+        $this->retrieve();
+        $this->persist();
+        return $this->return();
+    }
 
+    private function retrieve(){}
+
+    private function persist(){}
+
+    private function return():ProfitAndLossDTO {
+        //TODO::Load via Factory method - get "new" keyword out of business layer.
+        $dto = new ProfitAndLossDTO();
+        $dto->setSymbolProfitAndLoss('AAPL', -2.13);
+        $dto->setSymbolProfitAndLoss('MSFT', 4.19);
+
+        return $dto;
     }
 }
