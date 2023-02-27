65c05e6556149c9ea62b0ede6f90c59ffa50ff88
mcgettrm <mcgettrick.michael@gmail.com>
2023-02-27 19:12:10 +0800

    Resolving a dependency issue with adatper when registering with the service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\ProfitAndLoss\FinnhubAdapter;
 use App\ProfitAndLoss\ProfitAndLossFacade;
 use App\ProfitAndLoss\GrossProfitAndLossStrategy;
+use App\ProfitAndLoss\ProfitAndLossFactory;
 use App\ProfitAndLoss\StockDataReadingRepository;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -23,12 +24,16 @@ class AppServiceProvider extends ServiceProvider
                 $app->make(FinnhubAdapter::class),
                 $app->make(StockDataReadingRepository::class),
                 $app->make(GrossProfitAndLossStrategy::class),
+                $app->make(ProfitAndLossFactory::class)
             );
         });
 
         $this->app->bind(FinnhubAdapter::class,function($app){
             $finnhubToken = env('FINNHUB_API_KEY');
-            return new FinnhubAdapter($finnhubToken);
+            return new FinnhubAdapter(
+                $finnhubToken,
+                $app->make(ProfitAndLossFactory::class)
+            );
         });
     }
 
